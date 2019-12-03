5d53a1c8a3aee8c103960d0cc451d31f19732df1
arudkov14 <andrey14@ru.is>
2019-12-03 15:30:36 +0000

    Style for front page

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 import FileSystem from 'expo-file-system';
+import styles from './styles.js'
 
 import ContactsView from '../../views/ContactsView';
 import { getAllContacts, writeContacts, initContacts } from '../../services/contactService.js';
@@ -38,12 +39,12 @@ class Contacts extends React.Component {
 		const { contacts } = this.state;
 		return(
 			<View>
-				<FlatList
+				<FlatList style={ styles.createList}
 					numColumns={1} data={ contacts }
 					initialNumToRender={50}
 					renderItem={ ({ item: { id, name, phone }}) => {
 						return(
-							<Text> { name } </Text>
+							<Text style={ styles.createListText }> { name } </Text>
 						)
 					}}keyExtractor={ contact => contact.name}
 				/>
---
+++ src/components/Contacts/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	container: {
+    flex: 1,
+    backgroundColor: '#252c38',
+    justifyContent: 'center',
+  },
+
+	createList: {
+		marginBottom: 'auto',
+		backgroundColor: '#FFA500',
+		padding: 15,
+	},
+
+	createListText: {
+		color: '#fff',
+		alignSelf: 'flex-start',
+		fontSize: 20,
+	},
+
+});
