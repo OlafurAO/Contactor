668bf9a5308bc204169f1c5b95525de342405fd4
Olafur <olafurao16@ru.is>
2019-12-03 12:44:51 +0000

    contacts now visible in Contacts.js

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 import FileSystem from 'expo-file-system';
 
 import ContactsView from '../../views/ContactsView';
 import { getAllContacts, writeContacts, initContacts } from '../../services/contactService.js';
 
 class Contacts extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			contacts: '',
+		}
+	}
 	async componentWillMount() {
 		this.initContacts();
+		this.getAllContacts();
 	}
 
 	async initContacts() {
@@ -15,17 +22,43 @@ class Contacts extends React.Component {
   }
 
 	async getAllContacts() {
-		return await getAllContacts();
+		try{
+			let contacts = await getAllContacts();
+
+			this.setState({
+				contacts: contacts,
+			});
+			this.forceUpdate();
+		} catch (error) {
+      console.log(error.message);
+    }
 	}
 
 	render() {
+		const { contacts } = this.state;
 		return(
 			<View>
-				<TouchableOpacity>
-					<ContactsView contacts={ function(){ return getAllContacts() } }/>
-				</TouchableOpacity>
+				<FlatList
+					numColumns={1} data={ contacts }
+					initialNumToRender={50}
+					renderItem={ ({ item: { id, name, phone }}) => {
+						return(
+							<Text> { name } </Text>
+						)
+					}}keyExtractor={ contact => contact.name}
+				/>
 			</View>
 		);
+			/*
+			return(
+				<View>
+					<TouchableOpacity>
+						<ContactsView contacts={ contacts }/>
+					</TouchableOpacity>
+				</View>
+			);
+			*/
+
 	}
 }
 
--- src/views/ContactsView.js
+++ src/views/ContactsView.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 const ContactsView = (contacts) => {
-	async function ble() {
-		var c = await contacts.contacts();
-		console.log(c);
-	}
-	ble();
-	return(
+	console.log(contacts)
+
+	return (
 		<View>
-			<Text style={ { fontSize: 50, } }> ASJÆFLIJLKæsfdaaaaaaadslkjæf </Text>
+			<Text> KJHLASFKJHFAKJHAS </Text>
+		</View>
+	)
+
+	/*
+	return(
+		<View style={styles.container}>
+			<FlatList
+				numColumns={1}
+				data={ contacts }
+				initialNumToRender={50}
+				renderItem={ ({ item: { id, name, phone }}) => {
+					console.log(name);
+					return(
+						<Text style={{fontSize: 50}}> { name } </Text>
+					)
+				}}keyExtractor={ contact => contact.name}
+			/>
 		</View>
 	);
+	*/
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 'auto',
+  },
+});
 
 export default ContactsView;
